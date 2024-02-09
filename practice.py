bf3ec25bfebc4e448428a990a83a709ee773767f
yogamascu <yogamasc@gmail.com>
2024-02-09 11:40:14 +0700

    Practice 01

---
+++ practice.py
@@ -0,0 +1,11 @@
+#Test 1
+name = input (" Enter your name: ")
+name = name.split()
+firstname = name[0]
+jumlahkarakter = len(firstname)
+print('Total Character of your first name:', jumlahkarakter)
+
+
+#Test 2
+name = input (" Enter your name: ")
+
